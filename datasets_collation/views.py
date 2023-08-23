1d7f5cb864080df956b5fe444c222791b384f2d0
yesayagm <ygalatia@gmail.com>
2023-08-23 13:24:41 +0200

    modify index function to load form and read uploaded files

--- datasets_collation/views.py
+++ datasets_collation/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import MergeDatasetForm
+import pandas as pd
 
 # Create your views here.
 def index(request):
-    msg = 'Here you can collate your datasets'
-    return HttpResponse(msg)
+    if request.method == 'POST':
+        form = MergeDatasetForm(request.POST, request.FILES)
+        if form.is_valid():
+            dataset_1 = request.FILES['dataset_1']
+            dataset_2 = request.FILES['dataset_2']
+
+            if dataset_1.name.endswith('csv') & dataset_2.name.endswith('csv'):
+                df_1 = pd.read_csv(dataset_1)
+                df_2 = pd.read_csv(dataset_2)
+
+                return HttpResponse(df_1, df_2)
+            else:
+                return HttpResponse('Files must be csv')
+    else:
+        form = MergeDatasetForm()
+    return render(request, 'read_csv_dataset', {'form': form})
