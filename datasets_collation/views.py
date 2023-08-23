b2109f430cffd86ea1d4c07e3dd18ea1393aaa11
yesayagm <ygalatia@gmail.com>
2023-08-23 11:46:48 +0200

    create basic index for view

--- datasets_collation/views.py
+++ datasets_collation/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django import HttpResponse
 
 # Create your views here.
+def index(request):
+    msg = 'Here you can collate your datasets'
+    return HttpResponse(msg)
