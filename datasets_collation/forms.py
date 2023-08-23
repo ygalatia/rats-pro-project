240dade65485c67ea23422558ed01fa7f073a9f9
yesayagm <ygalatia@gmail.com>
2023-08-23 14:28:58 +0200

    add countried field in the form

--- datasets_collation/forms.py
+++ datasets_collation/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
 class MergeDatasetForm(forms.Form):
+    COUNTRY_CHOICES = [
+        ('Netherlands', 'Netherlands'),
+        ('United Kingdom', 'United Kingdom'),
+    ]
+
     dataset_1 = forms.FileField(label="Dataset 1")
     dataset_2 = forms.FileField(label="Dataset 2")
+    countries = forms.MultipleChoiceField(
+        choices=COUNTRY_CHOICES,
+        widget=forms.SelectMultiple(attrs={'class': 'select2-multi'})
+    )
