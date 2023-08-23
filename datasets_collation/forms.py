33edc60c552a395591bd5d5f476135b57a76082a
yesayagm <ygalatia@gmail.com>
2023-08-23 13:53:13 +0200

    fix typos in form

--- datasets_collation/forms.py
+++ datasets_collation/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class MergeDatasetForm(forms.Form):
-    dataset1 = forms.FileField(label="Dataset 1")
-    dataset2 = forms.FileField(label="Dataset 2")
+    dataset_1 = forms.FileField(label="Dataset 1")
+    dataset_2 = forms.FileField(label="Dataset 2")
