579ad12fd456e2a6971df4de54d16627833cc5f0
yesayagm <ygalatia@gmail.com>
2023-08-24 14:15:57 +0200

    add test for read_csv_as_df function

--- datasets_collation/tests.py
+++ datasets_collation/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .views import read_csv_as_df, filter_by_countries, left_merge_dataframe, rename_fields, generate_file_name, get_save_path
-import pandas as pd
+from pandas import DataFrame
+import os
 
 # Create your tests here.
 class DatasetCollationTests(TestCase):
@@ -9,11 +10,18 @@ class DatasetCollationTests(TestCase):
     
     def test_filter_by_countries(self):
         data = {'id': [1, 2, 3], 'first_name':['Anntoine', 'Brook', 'Casey'], 'country':['France', 'United Kingdom', 'Netherlands']}
-        df = pd.DataFrame(data)
+        df = DataFrame(data)
 
         selected_countries = ['Netherlands', 'United Kingdom']
         filtered_df = filter_by_countries(df, selected_countries)
 
         self.assertNotIn('Anntoine', filtered_df['first_name'])
         self.assertEqual(len(filtered_df), 2)
+    
+    def test_read_csv_as_df(self):
+        csv_file_path = os.path.join(os.path.dirname(__file__), 'test_data', 'sample_data.csv')
+        df = read_csv_as_df(csv_file_path)
+        self.assertEqual(len(df), 3)
+
+
 
