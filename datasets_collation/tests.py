ff571edf3a85fcabd1760bfafbe3f407baf03df4
yesayagm <ygalatia@gmail.com>
2023-08-24 13:44:23 +0200

    Iniate unit test file

--- datasets_collation/tests.py
+++ datasets_collation/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .views import read_csv_as_df, filter_by_countries, left_merge_dataframe, rename_fields, generate_file_name, get_save_path
+
+class DatasetCollationTests(TestCase):
+    def init_test():
+        return "Data Collation App Test"
 
 # Create your tests here.
